082628daf1527d753f6fdef26cc780337933f4b3
ErickIgles <erick.almeida2125@gmail.com>
2025-11-21 18:41:56 -0300

    feat: adiciona rotas de cadastro e login

--- src/usuarios/urls.py
+++ src/usuarios/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
+
 from . import views
 
 app_name = 'usuarios'
 
 urlpatterns = [
+    path(
+        'cadastro/',
+        views.CadastroUsuarioCreateView.as_view(),
+        name='cadastro'
+    ),
+    path(
+        'login/',
+        views.UsuarioLoginView.as_view(),
+        name='login'
+    )
 ]
