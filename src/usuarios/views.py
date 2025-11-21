210d935642f601081d4f08ce1edc24c9d6f36a78
ErickIgles <erick.almeida2125@gmail.com>
2025-11-21 18:44:01 -0300

    feat: adiciona views para cadastro e login de usuários

--- src/usuarios/views.py
+++ src/usuarios/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.models import User
+from django.views.generic import CreateView
 
-# Create your views here.
+from django.contrib.auth.views import LoginView
 
+from .forms import CadastroUsuarioForm, UsuarioLoginForm
+
+
+class CadastroUsuarioCreateView(CreateView):
+    
+    template_name = 'usuarios/cadastro.html'
+    model = User
+    form_class = CadastroUsuarioForm
+    success_url = reverse_lazy('usuarios:login')
+    
+
+class UsuarioLoginView(LoginView):
+    template_name = 'usuarios/login.html'
+    form_class = UsuarioLoginForm
+    redirect_authenticated_user = True
 
