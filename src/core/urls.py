d64b53f008da45e8a6fb03021c5a5dfbadcab99b
ErickIgles <erick.almeida2125@gmail.com>
2025-11-21 23:13:17 -0300

    feat: adiciona rota de home

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from . import views
 
 app_name = 'core'
 
 
 urlpatterns = [
-    
+    path(
+        '',
+        views.HomeView.as_view(),
+        name='home'
+    )
 ]
