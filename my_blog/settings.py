3a3c42eb876656fee4dbe579b6366297fb03b9d7
ErickIgles <erick.almeida2125@gmail.com>
2025-11-21 23:06:46 -0300

    chore: configura URLs de login e redirecionamentos no settings

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -139,3 +139,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'usuarios:login'
+LOGIN_REDIRECT_URL = 'core:home'
+LOGOUT_REDIRECT_URL = 'usuarios:logout'
