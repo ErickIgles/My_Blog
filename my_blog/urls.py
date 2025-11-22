3ac0d543e672c8132c628fcbbe35093436d36e44
ErickIgles <erick.almeida2125@gmail.com>
2025-11-21 21:40:05 -0300

    feat: adiciona urls do app core na rota principal

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
         'usuarios/',
         include('usuarios.urls')
     ),
+    path(
+        '',
+        include('core.urls')
+    ),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
