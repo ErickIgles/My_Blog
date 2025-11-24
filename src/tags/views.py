ee34a9ea5b5115cc86133ecffe36cd46b2bca227
ErickIgles <erick.almeida2125@gmail.com>
2025-11-24 17:50:08 -0300

    feat: adiciona view para listagem de tags

--- src/tags/views.py
+++ src/tags/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView, DetailView
+
+from .models import Tag
+
+
+class TagListView(ListView):
+    model = Tag
+    template_name = 'tags/lista.html'
+    context_object_name = 'tags'
+
