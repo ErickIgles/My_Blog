2a6ed90a9bf424ce075c7cca7d6a6468334bfb7c
ErickIgles <erick.almeida2125@gmail.com>
2025-11-24 18:56:48 -0300

    feat: adiciona modelo Post

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+from core.models import ModelBase
+from tags.models import Tag
+
+
+class Post(ModelBase):
+    autor = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Autor'
+    )
+    titulo = models.CharField(
+        verbose_name='Título',
+        max_length=200
+    )
+    conteudo = models.TextField(
+        verbose_name='Conteúdo',
+        blank=True
+    )
+    slug = models.SlugField(
+        unique=True,
+        db_index=True,
+        verbose_name='Slug'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        related_name='posts',
+        verbose_name='Tags'
+    )
+    
+    class Meta:
+        ordering = ['-modificado']
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+        
+    def __str__(self):
+        return self.titulo
