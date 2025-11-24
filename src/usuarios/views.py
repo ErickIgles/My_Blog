32c8e419bf6d6c34d547fba2756904a0ac7e5aa6
ErickIgles <erick.almeida2125@gmail.com>
2025-11-24 09:28:04 -0300

    feat: adiciona views de logout, perfil, atualizar dados, desativar conta e alteração de senha

--- src/usuarios/views.py
+++ src/usuarios/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
-from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import CreateView, DetailView, UpdateView
+from django.contrib.auth.views import LoginView, PasswordChangeView
+from django.contrib.auth import logout
+from django.contrib import messages
+from django.views import View
 
-from django.contrib.auth.views import LoginView
-
-from .forms import CadastroUsuarioForm, UsuarioLoginForm
+from .forms import CadastroUsuarioForm, UsuarioLoginForm, UsuarioUpdateForm, UsuarioPasswordChangeForm
 
 
 class CadastroUsuarioCreateView(CreateView):
-    
     template_name = 'usuarios/cadastro.html'
     model = User
     form_class = CadastroUsuarioForm
@@ -20,4 +23,57 @@ class UsuarioLoginView(LoginView):
     template_name = 'usuarios/login.html'
     form_class = UsuarioLoginForm
     redirect_authenticated_user = True
+
+
+class UsuarioLogoutView(LoginRequiredMixin, View):
+    
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        return redirect('usuarios:login')
+
+
+class UsuarioPerfilDetailView(LoginRequiredMixin, DetailView):
+    template_name = 'usuarios/perfil.html'
+    model = User
+    context_object_name = 'usuario'
+    
+    
+    def get_object(self):
+        return self.request.user
+
+
+class UsuarioUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    template_name = 'usuarios/editar.html'
+    model = User
+    form_class = UsuarioUpdateForm
+    success_url = reverse_lazy('usuarios:perfil')
+    success_message = 'Dados atualizados com sucesso!'
+    
+    def get_object(self):
+        return self.request.user
+
+
+class UsuarioDesativarView(LoginRequiredMixin, View):
+    
+    def post(self, request):
+        
+        user = request.user
+        user.is_active = False
+        user.save()
+        
+        logout(request)
+                
+        messages.success(request, 'Conta desativada com sucesso.')
+        return redirect('usuarios:login')
+
+
+class UsuarioPasswordUpdateView(LoginRequiredMixin, PasswordChangeView):
+    template_name = 'usuarios/alterar_senha.html'
+    form_class = UsuarioPasswordChangeForm
+    success_url = reverse_lazy('usuarios:perfil')
+    
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Senha atualizada com sucesso!')
+        return super().form_valid(form)
 
