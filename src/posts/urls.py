802437851dfafaf5633e9dc6d1180051a1f021cb
ErickIgles <erick.almeida2125@gmail.com>
2025-11-24 19:05:36 -0300

    feat: adiciona urls de views de cadastro de post, atualizar, deletar, listar e detalhes

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,8 +1,32 @@
 from django.urls import path
-
+from . import views
 
 app_name = 'posts'
 
 urlpatterns = [
-    
+    path(
+        '',
+        views.PostListView.as_view(),
+        name='lista'
+    ),
+    path(
+        'formulario/',
+        views.PostCreateView.as_view(),
+        name='criar'
+    ),
+    path(
+        '<slug:slug>/atualizar/',
+        views.PostUpdateView.as_view(),
+        name='atualizar'
+    ),
+    path(
+        '<slug:slug>/deletar/',
+        views.PostDeleteView.as_view(),
+        name='deletar'
+    ),
+    path(
+        '<slug:slug>/',
+        views.PostDetailView.as_view(),
+        name='detalhe'
+    )    
 ]
