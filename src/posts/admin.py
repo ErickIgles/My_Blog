5c84b8d91d316c45da6d906df75b64276d521f62
ErickIgles <erick.almeida2125@gmail.com>
2025-11-24 18:56:06 -0300

    chore: adiciona modelo Post ao admin

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'autor',
+        'titulo',
+        'conteudo',
+        'slug',
+        'get_tags',
+        'criado',
+        'modificado',
+        'origem_dados'
+    ]
+    
+    list_display_links = [
+        'autor',
+        'titulo'
+    ]
+    
+    search_fields = [
+        'autor',
+        'titulo'
+    ]
+    
+    list_filter = [
+        'autor',
+    ]
+    
+    
+    def get_tags(self, obj):
+        return ', '.join([tag.nome for tag in obj.tags.all()])
+
+    get_tags.short_description = 'Tags'
