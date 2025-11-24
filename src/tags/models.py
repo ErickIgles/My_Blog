371ba606ac932503f0942817790fdb54beaa5db4
ErickIgles <erick.almeida2125@gmail.com>
2025-11-24 17:49:23 -0300

    feat: adiciona modelo Tag

--- src/tags/models.py
+++ src/tags/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from core.models import ModelBase
+
+
+class Tag(ModelBase):
+    nome = models.CharField(
+        verbose_name='Nome',
+        max_length=50,
+        unique=True
+    )
+    slug = models.SlugField(
+        verbose_name='Slug',
+        max_length=50,
+        unique=True
+    )
+    
+    class Meta:
+        ordering = ['nome']
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+
+    def __str__(self):
+        return self.nome
