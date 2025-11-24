f44da56588b37df6623ba752c2b59ddd492498cf
ErickIgles <erick.almeida2125@gmail.com>
2025-11-24 17:48:16 -0300

    chore: adiciona modelo Tag ao admin

--- src/tags/admin.py
+++ src/tags/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    
+    list_display = [
+        'id',
+        'nome',
+        'slug',
+        'criado',
+        'modificado',
+        'origem_dados'
+    ]
+    
+    list_display_links = [
+        'nome',
+        'slug'
+    ]
+    
+    search_fields = (
+        'nome',
+        'slug',
+    )
+    
+    list_filter = (
+        'nome',
+        'slug',
+    )
+
