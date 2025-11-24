b11ede524375e375581eabbdeb48e366e05ec86f
ErickIgles <erick.almeida2125@gmail.com>
2025-11-24 17:51:34 -0300

    feat: adiciona url de listagem de tags

--- src/tags/urls.py
+++ src/tags/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
+from . import views
 
 app_name = 'tags'
 
 urlpatterns = [
-    
+    path(
+        '',
+        views.TagListView.as_view(),
+        name='tags'
+    ),
 ]
