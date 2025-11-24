29a1fd88fd1c8a4b8277b405bf1a52eb719fe65c
ErickIgles <erick.almeida2125@gmail.com>
2025-11-24 09:24:24 -0300

    feat: adiciona urls de logout, perfil, editar, desativar e alterar senha da conta do usuário

--- src/usuarios/urls.py
+++ src/usuarios/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
 
 from . import views
 
@@ -15,5 +14,30 @@ urlpatterns = [
         'login/',
         views.UsuarioLoginView.as_view(),
         name='login'
-    )
+    ),
+    path(
+        'logout/',
+        views.UsuarioLogoutView.as_view(),
+        name='logout'
+    ),
+    path(
+        'me/',
+        views.UsuarioPerfilDetailView.as_view(),
+        name='perfil'
+    ),
+    path(
+        'editar/',
+        views.UsuarioUpdateView.as_view(),
+        name='editar'
+    ),
+    path(
+        'desativar/',
+        views.UsuarioDesativarView.as_view(),
+        name='desativar'
+    ),
+    path(
+        'alterar_senha/',
+        views.UsuarioPasswordUpdateView.as_view(),
+        name='alterar_senha'
+    ),
 ]
