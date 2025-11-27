3f0b4df3876e7c4523fa238890c53c60dd32f2ed
ErickIgles <erick.almeida2125@gmail.com>
2025-11-26 21:18:05 -0300

    feat: adiciona listagem geral de posts, busca e páginação

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,29 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from django.db.models import Q
+
+from posts.models import Post
 
 
-class HomeView(LoginRequiredMixin, TemplateView):
+class HomeView(ListView):
+    model = Post
     template_name = 'core/home.html'
+    context_object_name = 'posts'
+    paginate_by = 6
+    
+    def get_queryset(self):
+        qs = Post.publicados.all()\
+            .select_related('autor')\
+            .prefetch_related('tags')
+
+        busca = self.request.GET.get('busca')
+        
+        if busca:
+            qs = qs.filter(
+                Q(titulo__icontains=busca),
+                Q(conteudo__icontains=busca)
+            )
+
+        return qs
+        
+        
+     
